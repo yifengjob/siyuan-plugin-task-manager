4dd908c937e65ac3ab3fdbacf8acf58d233ed26e
yifengjob <yifengjob@qq.com>
2026-04-01 16:01:54 +0800

    Delete utils directory

--- utils/index.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue';
-
-export function getDomByVueComponent(component) {
-    const div = document.createElement('div');
-    const app = createApp(component);
-    app.mount(div);
-    return div;
-}
